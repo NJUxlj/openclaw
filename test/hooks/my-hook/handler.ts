42c236fc0497ff8b5ef624551cf36f57368bc06d
wentai.hzy <huzhongyang@come-future.com>
2026-02-03 14:20:54 +0800

    update test

---
+++ test/hooks/my-hook/handler.ts
@@ -0,0 +1,12 @@
+import type { HookHandler } from "../../src/hooks/hooks.js";
+
+const handler: HookHandler = async (event) => {
+  if (event.type !== "command" || event.action !== "new") {
+    return;
+  }
+
+  console.log("[my-hook] Running!");
+  // Your logic here
+};
+
+export default handler;
